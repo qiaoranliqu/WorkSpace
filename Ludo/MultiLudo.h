2a10e79a72b3b9762c2d21df8f7739d979acc2af
Feng Zhe <857075272@qq.com>
2020-09-12 22:22:57 +0800

    Initial commit

---
+++ Ludo/MultiLudo.h
@@ -0,0 +1,8 @@
+#include "cstdlib"
+#include "Ludo/MinimalPerfectCuckoo/minimal_perfect_cuckoo.h"
+
+template<class Key, class Value, uint8_t VL = sizeof(Value) * 8, uint8_t DL = 0>
+class MultiLudo{
+	uint32_t Table_Num;
+};
+	
